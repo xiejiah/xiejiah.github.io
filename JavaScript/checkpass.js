3714f420f6f6a65ae410787796c4f3e8f76a1a23
xiejiah <127846700+xiejiah@users.noreply.github.com>
2024-06-07 17:50:34 +0800

    new

--- JavaScript/checkpass.js
+++
@@ -1,9 +0,0 @@
-const PASS_IN_KEY = 'PASS';
-function checkpass() {
-    const check = localStorage.getItem(PASS_IN_KEY);
-    if (!check) {
-        alert('请先获取通行证!');
-        window.location.href = 'http://xiejiah.github.io/pass.html';
-    }
-}
-checkpass();
