0b8df8f7f4f5012312d8a6296df2848449b1f320
xiejiah <127846700+xiejiah@users.noreply.github.com>
2024-06-07 18:03:20 +0800

    new

--- JavaScript/logout.js
+++ JavaScript/logout.js
@@ -3,18 +3,12 @@ const ADMIN_LOGGED_IN_KEY = 'adminLoggedIn';
 const PASS_IN_KEY = 'PASS';
         // 注销用户
 function logout() {
-    const isAdminLoggedIn = localStorage.getItem(ADMIN_LOGGED_IN_KEY) === 'true';
-
     localStorage.removeItem(LOGGED_IN_KEY);
     localStorage.removeItem(ADMIN_LOGGED_IN_KEY);
     localStorage.removeItem(PASS_IN_KEY);
-
     alert('已注销!');
-    if (isAdminLoggedIn) {
-        window.location.href = 'https://xiejiah.github.io/index.html'; 
-    } else {
-        window.location.href = 'https://xiejiah.github.io/index.html'; // 普通客户注销后重定向到首页
-    }
+    window.location.href = 'https://xiejiah.github.io/index.html'; // 普通客户注销后重定向到首页
+
 }
 
 logout();
